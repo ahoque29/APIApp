5a191725493650dc0bcf087a71bfd605b2e3a19b
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-27 22:57:44 +0000

    Added the three postcode api call from postcode For the first api call, turned the response into a JObject got the admin district from the JObject

--- APIClient/Program.cs
+++ APIClient/Program.cs
@@ -29,14 +29,35 @@ namespace APIClient
 
 			Console.WriteLine();
 
-			// Code obtained from postman
-			var client = new RestClient("https://api.postcodes.io/postcodes/EC2Y5AS");
+			//// Code obtained from postman
+			//var client = new RestClient("https://api.postcodes.io/postcodes/EC2Y5AS");
+			//client.Timeout = -1;
+			//var request = new RestRequest(Method.GET);
+			//request.AddHeader("Content-Type", "application/json");
+			//request.AddHeader("Cookie", "__cfduid=d4ab2ca91e79a617295e1f864acd2bf461614162765");
+			//IRestResponse response = client.Execute(request);
+			//Console.WriteLine(response.Content);
+
+			// Three postcodes POST methods
+			var client = new RestClient("https://api.postcodes.io/postcodes");
 			client.Timeout = -1;
-			var request = new RestRequest(Method.GET);
+			var request = new RestRequest(Method.POST);
 			request.AddHeader("Content-Type", "application/json");
 			request.AddHeader("Cookie", "__cfduid=d4ab2ca91e79a617295e1f864acd2bf461614162765");
+			request.AddParameter("application/json", "{\r\n\t\"postcodes\" : [\"OX49 5NU\", \"M32 0JG\", \"NE30 1DP\"]\r\n}", ParameterType.RequestBody);
 			IRestResponse response = client.Execute(request);
 			Console.WriteLine(response.Content);
+
+			// Making the response into a JObject
+			var jsonResponse = JObject.Parse(restResponse.Content);
+			Console.WriteLine("\nResponse content as a JObject");
+			Console.WriteLine(jsonResponse);
+
+			Console.WriteLine();
+
+			// getting the admin district from the JObject
+			var adminDistrict = jsonResponse["result"]["admin_district"];
+			Console.WriteLine($"Admin district: {adminDistrict}");
 		}
 	}
 }
