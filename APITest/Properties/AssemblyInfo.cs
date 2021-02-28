7f7849a2b2200e4fcfc7df40588c967b0b53c004
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-28 23:59:43 +0000

    Added a config file to get the base url Added a test class to test the service class

---
+++ APITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06ff68b2-0dd9-4b8d-ae39-c8c58f4674e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APITest/Tests/WhenTheSinglePostCodeServiceIsCalled_WithValidPostcode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using APIClient;
+
+namespace APITest.Tests
+{
+	public class WhenTheSinglePostCodeServiceIsCalled_WithValidPostcode
+	{
+		SinglePostCodeService _spcs = new SinglePostCodeService();
+
+		public WhenTheSinglePostCodeServiceIsCalled_WithValidPostcode()
+		{
+			_spcs.MakeRequest("EC2Y 5AS");
+		}
+
+		[Test]
+		public void StatusIs200()
+		{
+			Assert.That(_spcs.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+		}
+	}
+}
