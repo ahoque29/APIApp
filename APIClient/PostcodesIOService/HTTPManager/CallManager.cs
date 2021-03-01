bad0b11a7cc113d9eecbbbbdb7d3a6938ed5c9ce
Ashib Hoque <gaffingsiv@hotmail.com>
2021-03-01 10:38:57 +0000

    initialised the call manager class

--- APIClient/PostcodesIOService/HTTPManager/CallManager.cs
+++ APIClient/PostcodesIOService/HTTPManager/CallManager.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace APIClient.PostcodesIOService
 {
-	class CallManager
+	public class CallManager
 	{
+		readonly IRestClient _client;
+
+		public CallManager()
+		{
+			_client = new RestClient
+			{
+				BaseUrl = new Uri(AppConfigReader.BaseUrl)
+			};
+		}
+
+		/// <summary>
+		/// Defines and makes a requestfor info on a single postcode and stores the response.
+		/// </summary>
+		/// <param name="postcode"></param>
+		public async Task<string> MakePostcodeRequest(string postcode)
+		{
+			// Set up the request
+			var request = new RestRequest();
+			request.AddHeader("Content-Type", "application/json");
+
+			// Define request resource path
+			// Changing to lower case
+			// Removing any whitespace
+			request.Resource = $"postcodes/{postcode.ToLower().Replace(" ", "")}";
+
+			// Make the request
+			IRestResponse response = await _client.ExecuteAsync(request);
+
+			return response.Content;
+		}
 	}
 }
